4c23ab2630b72c9038ccbaf429fefcf4625de536
jesuscu96 <jclementeuroz@gmail.com>
2026-03-16 18:36:51 +0100

    telephone

--- pruebas.js
+++ pruebas.js
@@ -1,18 +1,64 @@
-/* function rot13(str) {
-  return str.replace(/[A-Z]/g, c =>
-    String.fromCharCode((c.charCodeAt(0) - 65 + 13) % 26 + 65)
-  );
-} */
-/* 
-charCodeAt(0) obtiene el código ASCII de la letra.
+function convertToRoman(num) {
+  const romanNumerals = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1]
+  ];
 
-code - 65 convierte A-Z a 0-25.
 
-+13 aplica el desplazamiento ROT13.
+  const filterNumbers = romanNumerals.filter(([v1, v2]) => v2 <= num);
+  
+  const sumNumbers = filterNumbers.reduce((acc, [v1, v2], index) => {
+    /* if (v2 <= num) {
+      acc = [...acc, [v1 += v1, v2 += v2]]
+      if (acc[index][1] <= num) {
+        acc = [...acc, [v1 += v1, v2 += v2]]
+        if (acc[index + 1][1] <= num) {
+          acc = [...acc, [v1 += v1, v2 += v2]]
+          return acc;
+        }
+        return acc;
+      }
+      return acc;
+    } */
+    if (index == 0) {
+      if (num <= (v2 * 3)) {
+        return acc = [...acc, [v1 + v1 + v1, v2 * 3]]
+      }
+      else if (num <= (v2 * 2)) {
+        return acc = [...acc, [v1 + v1, v2 * 2]]
+      } else if (num <= v2) {
+        return acc = [...acc, [v1, v2]]
+      }
 
-%26 evita salir del alfabeto.
+    }
+    else {
+      const sumAcc = acc.reduce((acc, [e1, e2])=> {acc + e2});
+      if (num <= ((v2 * 3) + sumAcc)) {
+        return acc = [...acc, [v1 + v1 + v1, v2 * 3]]
+      }
+      else if (num <= ((v2 * 2) + sumAcc)) {
+        return acc = [...acc, [v1 + v1, v2 * 2]]
+      } else if ((num <= v2) + sumAcc) {
+        return acc = [...acc, [v1, v2]]
+      }
+    }
 
-+65 vuelve al rango ASCII de A-Z.
 
-fromCharCode convierte el número nuevamente a letra.
-*/
+  }, [])
+  console.table(sumNumbers);
+
+
+}
+
+convertToRoman(36);
---
+++ telephone_number_validator.js
@@ -0,0 +1,6 @@
+function telephoneCheck(str) {
+  console.log(str.join(""));
+  
+}
+
+telephoneCheck("1 (555) 555-5555");
